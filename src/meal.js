abfe2b92ac2d36e2ac03e61c9e8c5f905048b429
RyanDBarnett <conner2468@gmail.com>
2022-11-25 14:06:51 -0700

    Completed meal and order files

--- src/meal.js
+++ src/meal.js
@@ -2,12 +2,36 @@ function nameMenuItem(item) {
   return "Delicious " + item;
 }
 
+function createMenuItem(name, price, type) {
+  return {name, price, type};
+}
+
+function addIngredients(ingredient, ingredients) {
+  for (var i = 0; i < ingredients.length; i++) {
+    if (ingredients[i] === ingredient) {
+      return;
+    }
+  }
+  ingredients.push(ingredient);
+}
+
+function formatPrice(price) {
+  return "$" + price;
+}
+
+function decreasePrice(price) {
+  return price - (price * .10);
+}
+
+function createRecipe(title, ingredients, type) {
+  return {title, ingredients, type};
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
--- src/order.js
+++ src/order.js
@@ -1,7 +1,38 @@
+function takeOrder(order, deliveryOrders) {
+  if (deliveryOrders.length < 3) {
+    deliveryOrders.push(order);
+  }
+}
+
+function refundOrder(orderNumber, deliveryOrders) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    var currentOrder = deliveryOrders[i];
+    if (currentOrder.orderNumber === orderNumber) {
+      deliveryOrders.splice(i, 1);
+    }
+  }
+}
+
+function listItems(deliveryOrders) {
+  var nameList = [];
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    nameList.push(deliveryOrders[i].item);
+  }
+  return nameList.join(', ');
+}
+
+function searchOrder(deliveryOrders, itemName) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].item === itemName) {
+      return true;
+    }
+  }
+  return false;
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
